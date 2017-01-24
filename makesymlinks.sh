3daceacc549c3d4990ab8419f2f731ea3033c50c
Ashton Baker <ashtonsb@umich.edu>
2017-01-24 10:52:19 -0500

    added bash_profile to dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc spacemacs"                 # list of files/folders to symlink in homedir
+files="bashrc bash_profile spacemacs"                 # list of files/folders to symlink in homedir
 #bashrc vimrc vim zshrc oh-my-"    
 
 ##########
