78479e9a5cbbb170e55257ac30316ff2cdf27020
Ashton Baker <ashtonsb@umich.edu>
2022-07-12 11:27:19 -0600

    add moonlander config

---
+++ moonlander/moonlander_ashton-s-layout_source/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"Da0JZ/DR4y7"
+#define RGB_MATRIX_STARTUP_SPD 60
