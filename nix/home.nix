a2dcd13d38aebdced06c73c2140f49f03019b227
Ashton Baker <ashton@localhost.localdomain>
2022-07-14 23:51:34 -0600

    add starship to home-manager config

--- nix/home.nix
+++ nix/home.nix
@@ -85,7 +85,40 @@
 
   
   ### Terminal configuration
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "ls -a";
+      ll = "ls -la";
+    };
+    shellAbbrs = {
+      o = "open";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[x](bold red)";
+        vicmd_symbol = "[<](bold green)";
+      };
+      git_commit = {
+        tag_symbol = " tag ";
+      };
+      git_status = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+    };
+    enableFishIntegration = true;
+  };
+
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
     font = {
