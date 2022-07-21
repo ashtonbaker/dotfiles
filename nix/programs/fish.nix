67f97db53ff39541fb489c31bcbb2d31a7260073
Ashton Baker <mail@ashtonbaker.com>
2022-07-21 12:58:29 -0600

    move git config into home-manager
    also, break out program config into files

---
+++ nix/programs/fish.nix
@@ -0,0 +1,65 @@
+{ pkgs, ... }:
+{
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "ls -aG";
+      ll = "ls -laG";
+    };
+    shellAbbrs = {
+      o = "open";
+    };
+    interactiveShellInit = "fish_add_path ~/.local/bin ~/bin ~/.nix-profile/bin";
+    plugins = [{
+        name="foreign-env";
+        src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-foreign-env";
+            rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+            sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+    }];
+
+    shellInit =
+    ''
+        # nix
+        if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+            fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        end
+
+        # home-manager
+        #if test -e <nix_file_path_file>
+        #    fenv source <nix_file_path_file>
+        #end
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      character = {
+        success_symbol = "[>](bold green)";
+        error_symbol = "[x](bold red)";
+        vicmd_symbol = "[<](bold green)";
+      };
+      git_commit = {
+        tag_symbol = " tag ";
+      };
+      git_status = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+    };
+    enableFishIntegration = true;
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+}
