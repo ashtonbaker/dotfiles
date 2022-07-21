494a0dc0e31105552741da4229a47728d88ecbc1
Ashton Baker <ashtonbaker@MacBook-Pro.localdomain>
2022-07-21 11:13:33 -0600

    fix fish config

--- nix/home-common.nix
+++ nix/home-common.nix
@@ -37,13 +37,35 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      ls = "ls -a";
-      ll = "ls -la";
+      ls = "ls -aG";
+      ll = "ls -laG";
     };
     shellAbbrs = {
       o = "open";
     };
-    interactiveShellInit = "fish_add_path ~/.local/bin ~/bin";
+    interactiveShellInit = "fish_add_path ~/.local/bin ~/bin ~/.nix-profile/bin";
+    plugins = [{
+        name="foreign-env";
+        src = pkgs.fetchFromGitHub {
+            owner = "oh-my-fish";
+            repo = "plugin-foreign-env";
+            rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+            sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+    }];
+
+    shellInit =
+    ''
+        # nix
+        if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+            fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        end
+
+        # home-manager
+        #if test -e <nix_file_path_file>
+        #    fenv source <nix_file_path_file>
+        #end
+    '';
   };
 
   programs.starship = {
@@ -66,6 +88,11 @@
         deleted = "x";
       };
     };
+    enableFishIntegration = true;
+  };
+
+  programs.atuin = {
+    enable = true;
     enableFishIntegration = true;
   };
 
