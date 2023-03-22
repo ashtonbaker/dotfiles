772df138b769c05892821a39f4bd1b8c74962b16
Ashton Baker <mail@ashtonbaker.com>
2023-03-22 08:01:29 -0600

    update doom config

--- nix/emacs/.doom.d/custom.el
+++ nix/emacs/.doom.d/custom.el
@@ -5,7 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("e838d6375a73fda607820c65eb3ea1f9336be7bd9a5528c9161e10c4aa663b5b" "e3c87e869f94af65d358aa279945a3daf46f8185f1a5756ca1c90759024593dd" default))
- '(package-selected-packages '(yaml-mode)))
+ '(package-selected-packages '(yaml-mode))
+ '(ignored-local-variable-values '((ssh-deploy-debug . 1)))
+ '(package-selected-packages '(yaml-mode))
+ '(safe-local-variable-values
+   '((ssh-deploy-async-with-threads . 1)
+     (ssh-deploy-force-on-explicit-save . 1)
+     (ssh-deploy-on-explicit-save . 1))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
