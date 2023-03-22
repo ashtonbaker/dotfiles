6a19cb0b24486029673489bb4428bc5eb3f0cd69
Ashton Baker <mail@ashtonbaker.com>
2023-03-22 07:59:27 -0600

    add emacs daemon to macos config

--- nix/home-macos.nix
+++ nix/home-macos.nix
@@ -3,6 +3,24 @@
   imports = [ ./home-common.nix ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "ashtonbaker";
-  home.homeDirectory = "/Users/ashtonbaker";
+  home.username = "ashton";
+  home.homeDirectory = "/Users/ashton";
+
+  launchd = {
+    enable = true;
+    agents = {
+      emacsclient = {
+        enable = true;
+        config = {
+          ProgramArguments = [
+            "/users/ashton/.nix-profile/bin/emacs"
+            "--daemon"
+          ];
+          RunAtLoad = true;
+          KeepAlive = true;
+          Label = "gnu.emacs.daemon";
+        };
+      };
+    };
+  };
 }
